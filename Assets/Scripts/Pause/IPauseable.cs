78514010ad25cbde7348e6667db4d8dd2f6640db
Egor Slesarenko <egor050503sl@gmail.com>
2023-09-18 10:22:52 +0300

    add zenject

---
+++ Assets/Scripts/Pause/IPauseable.cs
@@ -0,0 +1,4 @@
+﻿public interface IPauseable
+{
+    public bool SetPaused(bool paused);
+}
---
+++ Assets/Scripts/Pause/Pause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+public class Pause
+{
+    private static Pause _instance;
+    private List<IPauseable> _pauseables = new();
+
+    public static Pause Instance => _instance ?? new Pause();
+
+    public void Register(IPauseable pauseable)
+    {
+        _pauseables.Add(pauseable);
+    }
+
+    public void UnRegister(IPauseable pauseable)
+    {
+        _pauseables.Remove(pauseable);
+    }
+    
+    public void SetPause(bool paused)
+    {
+        foreach (IPauseable pauseable in _pauseables)
+        {
+            pauseable.SetPaused(paused);
+        }
+    }
+}
